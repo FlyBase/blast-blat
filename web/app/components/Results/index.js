afa422336638e68c4a52e6f6b532724bf27256d6
Josh Goodman <jogoodma@indiana.edu>
2016-07-16 09:19:22 -0400

    Moved Results from container to component.

---
+++ web/app/components/Results/index.js
@@ -0,0 +1,20 @@
+/**
+*
+* Results
+*
+*/
+
+import React from 'react';
+
+import styles from './styles.css';
+
+class Results extends React.Component {
+  render() {
+    return (
+      <div className={styles.results}>
+      </div>
+    );
+  }
+}
+
+export default Results;
